3e92e320170c44323818fafbb5362240916aa897
GabrielR19-G2 <79234237+GabrielR19-G2@users.noreply.github.com>
2023-10-31 00:52:50 -0700

    Presentacion pt1

---
+++ presentacion/presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -0,0 +1,12 @@
+package com.mycompany.presentacion;
+
+/**
+ *
+ * @author gabri
+ */
+public class Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
