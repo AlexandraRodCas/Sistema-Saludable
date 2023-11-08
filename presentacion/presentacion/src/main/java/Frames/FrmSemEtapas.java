24875c359ff92ad12e848138809d1d190f5d9925
ElisaVelasquez <101837234+ElisaVelasquez@users.noreply.github.com>
2023-11-08 14:16:55 -0700

    Pantalla y funcionalidad HU9
    Se calcula la cantidad de semanas en cada periodo.

--- presentacion/presentacion/src/main/java/Frames/FrmSemEtapas.java
+++ presentacion/presentacion/src/main/java/Frames/FrmSemEtapas.java
@@ -17,6 +17,32 @@ public class FrmSemEtapas extends javax.swing.JFrame {
         initComponents();
     }
 
+    public void calcularPeriodos() {
+        int eGen = 0, eEsp = 0, eP = 0, eC = 0;
+        
+        if(!(txtEtapaG.toString().isBlank())){
+            eGen = Integer.parseInt(txtEtapaG.getText());
+        }
+        if(!txtEtapaE.toString().isBlank()){
+            eEsp = Integer.parseInt(txtEtapaE.getText());
+        }
+        if(!txtEtapaP.toString().isBlank()){
+            eP = Integer.parseInt(txtEtapaP.getText());
+        }
+        if(!txtEtapaC.toString().isBlank()){
+            eC = Integer.parseInt(txtEtapaC.getText());
+        }
+        
+        
+        
+        int pP = eGen +eEsp;
+        int pC = eP + eC;
+
+        txtPerP.setText(String.valueOf(pP));
+        txtPerC.setText(String.valueOf(pC));
+
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,21 +52,295 @@ public class FrmSemEtapas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtEtapaG = new javax.swing.JTextField();
+        txtEtapaP = new javax.swing.JTextField();
+        txtEtapaE = new javax.swing.JTextField();
+        txtEtapaC = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        txtPerP = new javax.swing.JTextField();
+        txtPerC = new javax.swing.JTextField();
+        btnContinuar = new javax.swing.JToggleButton();
+        btnCancelar = new javax.swing.JToggleButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(0, 109, 182));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel1.setText("Etapa general");
+
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel2.setText("Etapa especial");
+
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel3.setText("Precompetitivo");
+
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel4.setText("Competitivo");
+
+        txtEtapaG.setText("0");
+        txtEtapaG.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEtapaGActionPerformed(evt);
+            }
+        });
+        txtEtapaG.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtEtapaGKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtEtapaGKeyTyped(evt);
+            }
+        });
+
+        txtEtapaP.setText("0");
+        txtEtapaP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEtapaPActionPerformed(evt);
+            }
+        });
+        txtEtapaP.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtEtapaPKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtEtapaPKeyTyped(evt);
+            }
+        });
+
+        txtEtapaE.setText("0");
+        txtEtapaE.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEtapaEActionPerformed(evt);
+            }
+        });
+        txtEtapaE.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtEtapaEKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtEtapaEKeyTyped(evt);
+            }
+        });
+
+        txtEtapaC.setText("0");
+        txtEtapaC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEtapaCActionPerformed(evt);
+            }
+        });
+        txtEtapaC.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtEtapaCKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtEtapaCKeyTyped(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel5.setText("Periodo competitivo");
+
+        jLabel6.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel6.setText("Periodo preparatorio");
+
+        txtPerP.setEditable(false);
+        txtPerP.setText("0");
+        txtPerP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPerPActionPerformed(evt);
+            }
+        });
+
+        txtPerC.setEditable(false);
+        txtPerC.setText("0");
+        txtPerC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPerCActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtEtapaG, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtEtapaC, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(txtEtapaP, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(30, 30, 30)
+                                        .addComponent(jLabel5))
+                                    .addComponent(jLabel6))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtPerP, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtPerC, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtEtapaE, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtEtapaG, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtEtapaE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPerP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addGap(22, 22, 22)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtEtapaP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtPerC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtEtapaC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(32, Short.MAX_VALUE))
+        );
+
+        btnContinuar.setBackground(new java.awt.Color(0, 109, 182));
+        btnContinuar.setText("Continuar");
+        btnContinuar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnContinuarActionPerformed(evt);
+            }
+        });
+
+        btnCancelar.setBackground(new java.awt.Color(0, 109, 182));
+        btnCancelar.setText("Cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(108, 108, 108)
+                        .addComponent(btnContinuar)
+                        .addGap(81, 81, 81)
+                        .addComponent(btnCancelar)))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnContinuar)
+                    .addComponent(btnCancelar))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtEtapaGActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEtapaGActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEtapaGActionPerformed
+
+    private void txtEtapaPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEtapaPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEtapaPActionPerformed
+
+    private void txtEtapaEActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEtapaEActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEtapaEActionPerformed
+
+    private void txtEtapaCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEtapaCActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEtapaCActionPerformed
+
+    private void txtPerPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPerPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtPerPActionPerformed
+
+    private void txtPerCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPerCActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtPerCActionPerformed
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void txtEtapaGKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaGKeyTyped
+        
+    }//GEN-LAST:event_txtEtapaGKeyTyped
+
+    private void txtEtapaEKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaEKeyTyped
+      
+    }//GEN-LAST:event_txtEtapaEKeyTyped
+
+    private void txtEtapaPKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaPKeyTyped
+       
+    }//GEN-LAST:event_txtEtapaPKeyTyped
+
+    private void txtEtapaCKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaCKeyTyped
+       
+    }//GEN-LAST:event_txtEtapaCKeyTyped
+
+    private void txtEtapaCKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaCKeyReleased
+       
+    }//GEN-LAST:event_txtEtapaCKeyReleased
+
+    private void txtEtapaPKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaPKeyReleased
+       
+    }//GEN-LAST:event_txtEtapaPKeyReleased
+
+    private void txtEtapaEKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaEKeyReleased
+
+    }//GEN-LAST:event_txtEtapaEKeyReleased
+
+    private void txtEtapaGKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEtapaGKeyReleased
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_txtEtapaGKeyReleased
+
+    private void btnContinuarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnContinuarActionPerformed
+        // TODO add your handling code here:
+        this.calcularPeriodos();
+        
+    }//GEN-LAST:event_btnContinuarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +378,20 @@ public class FrmSemEtapas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton btnCancelar;
+    private javax.swing.JToggleButton btnContinuar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtEtapaC;
+    private javax.swing.JTextField txtEtapaE;
+    private javax.swing.JTextField txtEtapaG;
+    private javax.swing.JTextField txtEtapaP;
+    private javax.swing.JTextField txtPerC;
+    private javax.swing.JTextField txtPerP;
     // End of variables declaration//GEN-END:variables
 }
