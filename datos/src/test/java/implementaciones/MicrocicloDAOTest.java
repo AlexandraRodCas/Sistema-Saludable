c240987aee924a428fc60a3ef6ba62f5fc03e736
ElisaVelasquez <101837234+ElisaVelasquez@users.noreply.github.com>
2023-12-07 21:12:11 -0700

    Pruebas JUnit HU7

---
+++ datos/src/test/java/implementaciones/MicrocicloDAOTest.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package implementaciones;
+
+import entidades.Etapa;
+import entidades.Microciclo;
+import java.sql.Date;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author velas
+ */
+public class MicrocicloDAOTest {
+
+    public MicrocicloDAOTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of agregarMicrociclo method, of class MicrocicloDAO.
+     */
+    @Test
+    public void testAgregarMicrociclo() {
+        System.out.println("agregarMicrociclo");
+        Microciclo microciclo = new Microciclo();
+        Date date1 = new Date(2023, 12, 7);
+        Date date2 = new Date(2024, 3, 7);
+        microciclo.setInicio(date1);
+        microciclo.setFin(date2);
+        microciclo.setNoMesociclo(1);
+        microciclo.setCiclicidad("10,3");
+        Etapa etapa = new Etapa();
+        microciclo.setEtapa(etapa);
+        MicrocicloDAO instance = new MicrocicloDAO();
+        boolean result = instance.agregarMicrociclo(microciclo);
+        if (!result) {
+            fail("No se pudo agregar el microciclo.");
+
+        }
+        assertNotNull(result);
+    }
+
+    /**
+     * Test of consultarMicrociclos method, of class MicrocicloDAO.
+     */
+    @Test
+    public void testConsultarMicrociclos() {
+        System.out.println("consultarMicrociclos");
+        MicrocicloDAO instance = new MicrocicloDAO();
+        List<Microciclo> expResult = null;
+        List<Microciclo> result = instance.consultarMicrociclos();
+        if(result.isEmpty()){
+            fail("No se pudo consultar los microciclos");
+        }
+    }
+
+    /**
+     * Test of consultarMicrocicloId method, of class MicrocicloDAO.
+     */
+    @Test
+    public void testConsultarMicrocicloId() {
+        System.out.println("consultarMicrocicloId");
+        int id = 0;
+        MicrocicloDAO instance = new MicrocicloDAO();
+        Microciclo result = instance.consultarMicrocicloId(id);
+        if(result.getId()!=id){
+            fail("No se pudo consultar el microciclo.");
+        }
+        assertNotNull(result);
+    }
+
+}
---
+++ negocio_saludable/negocio/src/test/java/controles/ControlCiclicidadTest.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package controles;
+
+import entidades.Etapa;
+import entidades.Microciclo;
+import java.sql.Date;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author velas
+ */
+public class ControlCiclicidadTest {
+    
+    public ControlCiclicidadTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of fechaInicial method, of class ControlCiclicidad.
+     */
+    @Test
+    public void testFechaInicial() {
+        System.out.println("consultaEtapas");
+        ControlCiclicidad instance = new ControlCiclicidad();
+        List<Etapa> result = instance.consultaEtapas();
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+    }
+
+    /**
+     * Test of consultaEtapas method, of class ControlCiclicidad.
+     */
+    @Test
+    public void testConsultaEtapas() {
+        System.out.println("consultaEtapas");
+        ControlCiclicidad instance = new ControlCiclicidad();
+        List<Etapa> result = instance.consultaEtapas();
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+    }
+
+    /**
+     * Test of agregarMicrociclo method, of class ControlCiclicidad.
+     */
+    @Test
+    public void testAgregarMicrociclo() {
+        System.out.println("Test agregarMicrociclo: ");
+        int cantidad = 1;
+        String ciclicidad = "1,3";
+        int mesociclo = 1; 
+        String etapa = "Preparatoria";
+
+        ControlCiclicidad instance = new ControlCiclicidad();
+        instance.agregarMicrociclo(cantidad, ciclicidad, mesociclo, etapa);
+
+        
+        Microciclo lastMicrociclo = instance.consultarUltimoMicroAgregado();
+        assertNotNull(lastMicrociclo);
+        assertEquals(ciclicidad, lastMicrociclo.getCiclicidad());
+        assertEquals(mesociclo, lastMicrociclo.getNoMesociclo());
+        assertEquals(etapa, lastMicrociclo.getEtapa().getTipo());
+    }
+
+    /**
+     * Test of consultarUltimoMicroAgregado method, of class ControlCiclicidad.
+     */
+    @Test
+    public void testConsultarUltimoMicroAgregado() {
+        System.out.println("Test consultarUltimoMicroAgregado");
+        ControlCiclicidad instance = new ControlCiclicidad();
+        Microciclo result = instance.consultarUltimoMicroAgregado();
+        assertNull(result); 
+    }
+    
+}
